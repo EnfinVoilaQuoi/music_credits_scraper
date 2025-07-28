0fb9595cee516a0798785bd3a928907a4aeccda6
EnfinVoilaQuoi <g78rem@gmail.com>
2025-07-28 18:25:50 +0200

    Nettoyage

--- Lancer_Venv_Admin.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Lancer PowerShell en mode administrateur avec activation du venv
-
-:: Définir le chemin du script PowerShell
-set SCRIPT_PATH=%~dp0Lancer_Venv_Admin.ps1
-
-:: Lancer PowerShell avec élévation (admin), sans fermer après
-powershell -Command "Start-Process powershell -ArgumentList '-NoExit','-ExecutionPolicy Bypass','-File \"%SCRIPT_PATH%\"' -Verb RunAs"

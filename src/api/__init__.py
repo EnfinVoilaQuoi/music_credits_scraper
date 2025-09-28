dbf206b36f3f26a4991525dff48c126d117b3a61
EnfinVoilaQuoi <g78rem@gmail.com>
2025-09-28 20:20:13 +0200

    Up ReccoBeats Selenium

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,4 +4,4 @@ from .genius_api import GeniusAPI
 from .reccobeats_api import ReccoBeatsIntegratedClient
 from .snep_certifications import SNEPCertificationManager
 
-__all__ = ['GeniusAPI', 'ReccoBeatsClient', 'SNEPCertificationManager']
+__all__ = ['GeniusAPI', 'ReccoBeatsIntegratedClient', 'SNEPCertificationManager']
